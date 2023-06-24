ffe78f1bf140542f7eb8c077ef6e700c04a28410
WZRD <128477299+wzrdx@users.noreply.github.com>
2023-06-24 15:51:39 +0300

    iMac wallet

--- interactions/snippets.sh
+++ interactions/snippets.sh
@@ -9,8 +9,8 @@
 TRAVELERS_ID="TRAVELER-51bdef"
 
 ### mainnet
-USER_PEM="~/elrond-wallet/wallet-homex.pem"
-# USER_PEM="~/Crypto/wallet/wallet-homex.pem"
+# USER_PEM="~/elrond-wallet/wallet-homex.pem"
+USER_PEM="~/Crypto/wallets/wallet-homex.pem"
 PROXY="https://api.multiversx.com"
 CHAIN_ID="1"
 SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
