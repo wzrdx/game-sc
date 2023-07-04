e7122ca0953ad3c453cc47341de164487ba64030
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-07-04 16:20:05 +0300

    Airdrop function

--- interactions/snippets.sh
+++ interactions/snippets.sh
@@ -1,18 +1,18 @@
 ### devnet
-USER_PEM="~/elrond-wallet/wallet.pem"
-# USER_PEM="~/Crypto/wallets/wallet-kzcpl.pem"
-PROXY="https://devnet-api.multiversx.com"
-CHAIN_ID="D"
-SC_ADDRESS=erd1qqqqqqqqqqqqqpgq03qfld7ypk27r2k0wgux89573pw2htq8ukrqze9mpw
+# USER_PEM="~/elrond-wallet/wallet.pem"
+# # USER_PEM="~/Crypto/wallets/wallet-kzcpl.pem"
+# PROXY="https://devnet-api.multiversx.com"
+# CHAIN_ID="D"
+# SC_ADDRESS=erd1qqqqqqqqqqqqqpgq03qfld7ypk27r2k0wgux89573pw2htq8ukrqze9mpw
 
 TRAVELERS_ID="TRAVELER-51bdef"
 
 ### mainnet
-# USER_PEM="~/elrond-wallet/wallet-homex.pem"
-# # USER_PEM="~/Crypto/wallets/wallet-homex.pem"
-# PROXY="https://api.multiversx.com"
-# CHAIN_ID="1"
-# SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
+USER_PEM="~/elrond-wallet/wallet-homex.pem"
+# USER_PEM="~/Crypto/wallets/wallet-homex.pem"
+PROXY="https://api.multiversx.com"
+CHAIN_ID="1"
+SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
 
 
 deploy() {
