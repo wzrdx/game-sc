9aa1988c06a904642f39b2928c067fd0a2058281
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-10-24 17:02:52 +0300

    getXpLeaderboard

--- interactions/snippets.sh
+++ interactions/snippets.sh
@@ -23,9 +23,17 @@ upgrade() {
     mxpy contract build && mxpy --verbose contract upgrade ${SC_ADDRESS} --metadata-payable --metadata-payable-by-sc \
     --recall-nonce --pem=${USER_PEM} \
     --bytecode="./output/game-sc.wasm" \
-    --gas-limit=140000000 \
+    --gas-limit=160000000 \
     --send --outfile="upgrade.interaction.json" \
     --proxy=${PROXY} --chain=${CHAIN_ID} || return
+}
+
+airdropXp() {
+    mxpy --verbose contract call ${SC_ADDRESS} \
+    --proxy=${PROXY} --chain=${CHAIN_ID} \
+    --send --recall-nonce --pem=${USER_PEM} \
+    --gas-limit=10000000 \
+    --function="airdropXp"
 }
 
 drawRaffleWinners() {
