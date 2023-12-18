b73e12e9dd9e9a10090db82c3895808dd77e7ed1
wzrdx <128477299+wzrdx@users.noreply.github.com>
2023-12-19 00:54:06 +0200

    Competitions

--- src/competitions.rs
+++ src/competitions.rs
@@ -51,31 +51,8 @@ pub trait Competitions: storage::Storage + helpers::Helpers {
         let winner_address = self.raffle_id_participant(winner_id).get();
 
         // if let 1..=5 = raffle_id
-        if raffle_id == 96 {
-            self.send().direct_egld(&winner_address, &BigUint::from(750_000_000_000_000_000 as u64));
-        } else if raffle_id == 97 {
-            self.send().direct_egld(&winner_address, &BigUint::from(750_000_000_000_000_000 as u64));
-        } else if raffle_id == 98 {
-            self.send().direct_esdt(
-                &winner_address,
-                &TokenIdentifier::from(&b"HSWOLF-76e8bf"[..]),
-                738 as u64,
-                &BigUint::from(1 as u32),
-            );
-        } else if raffle_id == 99 {
-            self.send().direct_esdt(
-                &winner_address,
-                &TokenIdentifier::from(&b"HSWEREWOLF-a485c7"[..]),
-                2434 as u64,
-                &BigUint::from(1 as u32),
-            );
-        } else if raffle_id == 100 {
-            self.send().direct_esdt(
-                &winner_address,
-                &TokenIdentifier::from(&b"NFTIM-4586ac"[..]),
-                3317 as u64,
-                &BigUint::from(1 as u32),
-            );
+        if let 101..=105 = raffle_id {
+            self.send().direct_egld(&winner_address, &BigUint::from(500_000_000_000_000_000 as u64));
         }
 
         let hash: ManagedByteArray<Self::Api, 32> = self.blockchain().get_tx_hash();
