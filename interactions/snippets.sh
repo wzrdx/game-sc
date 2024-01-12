9d60f31675ce45ee41ad4712a6c9af09d6302138
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-01-12 13:12:04 +0200

    Snippets

--- interactions/snippets.sh
+++ interactions/snippets.sh
@@ -1,22 +1,22 @@
 ## devnet
 # USER_PEM="~/elrond-wallet/wallet.pem"
-# # USER_PEM="~/Crypto/wallets/wallet-kzcpl.pem"
-# PROXY="https://devnet-api.multiversx.com"
-# CHAIN_ID="D"
-# SC_ADDRESS=erd1qqqqqqqqqqqqqpgqc8s6t5594e4en4ffl60r6hn52hajkpkkukrqww29av
+USER_PEM="~/Crypto/wallets/wallet-kzcpl.pem"
+PROXY="https://devnet-api.multiversx.com"
+CHAIN_ID="D"
+SC_ADDRESS=erd1qqqqqqqqqqqqqpgqc8s6t5594e4en4ffl60r6hn52hajkpkkukrqww29av
 
 # OWNER=erd1za7d0lzgnee39p9sytre0mss76tnht70fem0pcv0zn4undcfukrqqkzcpl
 # PLAYER=
 
 ## mainnet
-USER_PEM="~/elrond-wallet/wallet-homex.pem"
-# USER_PEM="~/Crypto/wallets/wallet-homex.pem"
-PROXY="https://api.multiversx.com"
-CHAIN_ID="1"
-SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
+# USER_PEM="~/elrond-wallet/wallet-homex.pem"
+# # USER_PEM="~/Crypto/wallets/wallet-homex.pem"
+# PROXY="https://api.multiversx.com"
+# CHAIN_ID="1"
+# SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
 
-ART_COLLECTION_NAME="ArtOfMenhir"
-ART_TICKER="AOM"
+# ART_COLLECTION_NAME="ArtOfMenhir"
+# ART_TICKER="AOM"
 
 
 upgrade() {
