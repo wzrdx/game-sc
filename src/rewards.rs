8cbdccf0bda8571531c0848ace3d86853e72321f
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-01-23 23:29:20 +0200

    Optimize getStakingSummary

--- src/rewards.rs
+++ src/rewards.rs
@@ -8,13 +8,11 @@ pub trait Rewards: storage::Storage + helpers::Helpers {
     #[endpoint(claimReward)]
     fn claim_reward(&self) {
         let caller = self.blockchain().get_caller();
+        let elders_id = self.elders_mapper().get_token_id();
 
         let mut amount: usize = 0;
 
-        for nonce in self
-            .get_staked_nonces(&caller, self.elders_mapper().get_token_id())
-            .iter()
-        {
+        for nonce in self.get_staked_nonces(&caller, &elders_id).iter() {
             if self.elders_rewards_nonces().contains(&(nonce as u16)) {
                 amount += 2;
                 self.elders_rewards_nonces().swap_remove(&(nonce as u16));
@@ -28,8 +26,9 @@ pub trait Rewards: storage::Storage + helpers::Helpers {
     #[view(getElderRewards)]
     fn get_elder_rewards(&self, user: &ManagedAddress) -> usize {
         let mut tickets_amount: usize = 0;
+        let elders_id = self.elders_mapper().get_token_id();
 
-        for nonce in self.get_staked_nonces(user, self.elders_mapper().get_token_id()).iter() {
+        for nonce in self.get_staked_nonces(user, &elders_id).iter() {
             if self.elders_rewards_nonces().contains(&(nonce as u16)) {
                 tickets_amount += 1;
             }
