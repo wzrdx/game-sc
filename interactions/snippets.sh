0c1a5a6d2536104749ec6e7190646a63c0cda879
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-01-27 18:01:00 +0200

    Snippets & wallets

--- interactions/snippets.sh
+++ interactions/snippets.sh
@@ -9,8 +9,8 @@
 # PLAYER=
 
 ## mainnet
-# USER_PEM="~/elrond-wallet/wallet-homex.pem"
-USER_PEM="~/Crypto/wallets/wallet-homex.pem"
+USER_PEM="~/wallets/wallet-homex.pem"
+# USER_PEM="~/Crypto/wallets/wallet-homex.pem"
 PROXY="https://api.multiversx.com"
 CHAIN_ID="1"
 SC_ADDRESS=erd1qqqqqqqqqqqqqpgqpt68cy4cde6ff2wzcfsfncjv6gxjxda8dn7q9ekje9
@@ -67,7 +67,7 @@ refreshElderRewards() {
     mxpy --verbose contract call ${SC_ADDRESS} \
     --proxy=${PROXY} --chain=${CHAIN_ID} \
     --send --recall-nonce --pem=${USER_PEM} \
-    --gas-limit=30000000 \
+    --gas-limit=40000000 \
     --function="refreshElderRewards"
 }
 
