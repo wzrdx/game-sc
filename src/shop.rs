e1c308acecfcb3e43df482fabf35eb61f945625e
wzrdx <128477299+wzrdx@users.noreply.github.com>
2024-02-07 15:07:34 +0200

    Remove shop

--- src/shop.rs
+++ src/shop.rs
@@ -6,17 +6,4 @@ use crate::{helpers, storage};
 pub trait Shop:
     multiversx_sc_modules::default_issue_callbacks::DefaultIssueCallbacksModule + storage::Storage + helpers::Helpers
 {
-    // TODO:
-    #[only_owner]
-    #[endpoint(mint)]
-    fn mint(&self, amount: usize, nonce: u64) {
-        let caller = self.blockchain().get_caller();
-
-        self.art_mapper()
-            .nft_add_quantity_and_send(&caller, nonce, BigUint::from(amount));
-
-        self.legendary_char_aetheris(&caller).update(|i| {
-            *i += amount;
-        });
-    }
 }
